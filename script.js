a8aba882f97a7719d843d916380b7a77904fd3b8
M.A. Basar <basarbr7@gmail.com>
2025-05-01 23:18:57 +0600

    added fontendExam Ai agency

---
+++ script.js
@@ -0,0 +1,41 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+  
+    menuToggle.addEventListener("click", function () {
+      mobileMenu.classList.toggle("hidden");
+    });
+  });
+  
+
+//   const navbar = document.querySelector("#nav")
+//   window.addEventListener("scroll", (e)=>{
+//     let scrolly = window.scrollY
+//     if(scrolly>100){
+//       navbar.classList.add("fixed", "top-0", "header-animation")
+//       navbar.classList.remove("absolute");
+//     }else{
+//       navbar.classList.remove("fixed", "top-0", "header-animation")
+//       navbar.classList.add("absolute");
+//     }
+    
+//   })
+
+   // Smooth scroll 
+   const anchorsAll = document.querySelectorAll('a[href^="#"]')
+
+   anchorsAll.forEach(anchor => {
+     anchor.addEventListener('click', function (e) {
+       e.preventDefault();
+       const target = document.querySelector(this.getAttribute('href'));
+       const navbarHeight = 60;
+       const top = target.getBoundingClientRect().top + window.scrollY - navbarHeight;
+       
+       window.scrollTo({
+         top,
+         behavior: 'smooth'
+       });
+     });
+   });
